585c5d195258dfc4a835ba57a7856bea2ccb02e2
Thomas <96282112+jumpburce@users.noreply.github.com>
2022-12-09 09:20:16 +0800

    Update manager_menu_ui.h

--- inc/manager_menu_ui.h
+++ inc/manager_menu_ui.h
@@ -1,7 +1,15 @@
 //
 // Created by chen yuzhuo on 2022/12/9.
 //
-
+/**
+  **************************** AssessmentUI: manager_ui.h ****************************
+  * Created by thomas on 2022/12/9.
+  * @file
+  * @author     Thomas
+  * @date       2022-12-09
+  * @brief      The UI for manager
+  **************************** AssessmentUI: manager_ui.h ****************************
+ */
 #ifndef MANAGER_MENU_MANAGER_MENU_UI_H
 #define MANAGER_MENU_MANAGER_MENU_UI_H
 /**
