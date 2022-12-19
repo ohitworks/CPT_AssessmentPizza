5acfff9aacaa2008276d9ed20b336f5eb9492ea0
Miao <97503252+Miiiaao@users.noreply.github.com>
2022-12-19 23:56:31 +0800

    Update gen_ID.h
    Changed the char not adding bug to string not tested

--- inc/gen_ID.h
+++ inc/gen_ID.h
@@ -12,17 +12,21 @@
 #define ASSESSMENTPIZZA_GEN_ID_H
 
 #include "time.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 int count = 0;
 
 char *gen_id();
-char int_to_char(int object);
+char *int_to_string(int object);
 
 struct Type{
-    char ID_mon;
-    char ID_day;
-    char ID_hour;
-    char ID_min;
-    char ID_sec;
+    char *ID_year;
+    char *ID_mon;
+    char *ID_day;
+    char *ID_hour;
+    char *ID_min;
+    char *ID_sec;
 };
 #endif //ASSESSMENTPIZZA_GEN_ID_H
